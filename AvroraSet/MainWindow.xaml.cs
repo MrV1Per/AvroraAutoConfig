54d2578762e1103713fbc7a7066c5b0412620a32
Mr V!Per <vlad.movshan@gmail.com>
2022-11-22 16:44:13 +0200

    Добавьте файлы проекта.

---
+++ AvroraSet/MainWindow.xaml.cs
@@ -0,0 +1,295 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Net;
+using System;
+using System.IO;
+using System.Diagnostics;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using System.Threading;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AvroraSet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string dns = fieldDns.Text;
+            string corpusNum = fieldNum.Text;
+            string fieldInputIP = fieldIP.Text;
+
+            string newDNS = dns.Replace('з', 'p'); 
+
+            //Получить номер магазина и номер ПЧ из введенного ДНС имени
+            Regex regexNumTT = new Regex(@"\d{4}");
+            Regex regexNumPreice = new Regex(@"p\d{1,2}");
+            string priceNum = regexNumPreice.Matches(newDNS)[0].ToString().Remove(0, 1);
+            string numTT = regexNumTT.Matches(newDNS)[0].ToString();
+
+            try
+            {               
+                string ip;
+                if (SecondVariantCB.IsChecked == true)
+                {
+
+                    //Значения последниго сегмента ИП адреса прайсчекеров (1-50)
+                    string[] segmentIP = { "", "50", "51", "52", "53", "54", "55", "56", "57", "58", "80", 
+                                             "81", "82", "83", "84", "85", "86", "87", "88", "89", "90" ,
+                                             "91", "92", "93", "94", "95", "96", "97", "98", "99","200", 
+                                             "201", "202", "203", "204", "205", "206", "207", "208", "209", "210",
+                                             "211", "212", "213", "214", "215", "216", "217", "218", "219", "220" };
+                    string routerIp = Dns.GetHostEntry("a" + numTT + "-router").AddressList[0].ToString();
+
+                    routerIp = routerIp.Remove(routerIp.Length - 1);
+                    Int32.TryParse(priceNum, out int pNum);
+                    ip = routerIp + segmentIP[pNum];
+
+                }
+                else if (ipCB.IsChecked == true)                      
+                    ip = Dns.GetHostEntry(newDNS).AddressList[0].ToString(); //айпишник                   
+                else
+                    ip = fieldInputIP;
+                
+
+                for (; ; )
+                {
+                    if (numTT.StartsWith("0")) numTT = numTT.TrimStart('0');
+                    break;
+                }
+
+
+                new Window1(numTT, priceNum.ToString(), ip, corpusNum, newDNS).ShowDialog();
+
+            }
+            catch(System.Net.Sockets.SocketException)
+            {
+                MessageBox.Show("Нужный DNS адрес не найден, возможно не правильно введена информация, или запрашиваемый DNS не прописан в сети", "ERROR");
+                //System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+                //Application.Current. Shutdown();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Закрытие приложения
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Возможность двигать окно за топ меню
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Top_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        /// <summary>
+        /// Открытие файла с логами работы приложения
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Button_Log(object sender, RoutedEventArgs e)
+        {
+            Process.Start("notepad.exe", @"..\AvroraAutoConfig\log.txt");
+        }
+
+        /// <summary>
+        /// Внести данные в файл конфига
+        /// </summary>
+        /// <param name="line">Строка считаная с конфига</param>
+        /// <param name="ip">ИП адрес устройства</param>
+        /// <param name="gateway">Шлюз</param>
+        /// <returns></returns>
+        private string ParseInputCFG(string line, string ip, string gateway)
+        {
+            return line.Replace("{IP}", ip)
+                .Replace("{DNS}", fieldDns.Text)
+                .Replace("{GATEWAY}", gateway)
+                .Replace("{SERIAL}", SerialNum.Text); //ИП
+
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки "Orange"
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Orange_Click(object sender, RoutedEventArgs e)
+        {
+            //Данные для подключения по SSH
+            string user = "pi";
+            //string passDoor = GetPassByLogin(user);   :TODO
+            string passMusic = "";
+            string host = fieldLocalIP.Text;
+
+
+            string pathToConfigTemplateOrange = @"..\AvroraAutoConfig\music.py"; //Путь к шаблонному файлу конига оранжа
+            List<string> musicCfg = new List<string>();
+
+            using (StreamReader sr = new StreamReader(pathToConfigTemplateOrange, Encoding.Default))
+            {
+                string ip = Dns.GetHostEntry(fieldDns.Text).AddressList[0].ToString(); //Получить ИП адрес по ДНС имени
+                string gateway = ip.Remove(ip.Length - 2) + "1";
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = ParseInputCFG(line, ip, gateway);
+                    musicCfg.Add(line);
+                }
+            }
+
+
+            string pathOut = @"..\AvroraAutoConfig\musicFull.py"; //Путь к исходящему файлу измененного конфига
+            using (StreamWriter wr = new StreamWriter(pathOut, false, System.Text.Encoding.Default))//создаем файл с полним конфигом
+            {
+                int i = 0;
+                while (i < musicCfg.Count)
+                {
+                    wr.WriteLine(musicCfg[i]);
+                    i++;
+                }
+
+            }
+
+            //создаем sftp подключение, загружаем созданый файл на устрофство
+            using (ScpClient client = new ScpClient(host, user, passMusic))
+            {
+                client.Connect();
+                Thread.Sleep(200);
+                using (Stream localFile = File.OpenRead(pathOut))
+                {
+                    client.Upload(localFile, "/home/pi/orangeAuto.py");
+                }
+
+            }
+
+            //Подключение по SSH, и запуск скрипта для настройки
+            using (var client = new SshClient(host, user, passMusic))
+            {
+                string dns = fieldDns.Text;
+                string newDNS = dns.Replace('з', 'p');
+                //Start the connection
+                try
+                {
+                    client.Connect();
+                    Thread.Sleep(1000);
+                    client.RunCommand("echo 'Pi@)!^' | sudo -S python3 orangeAuto.py");
+
+                    Thread.Sleep(200);
+                    client.RunCommand("rm orangeAuto.py");
+
+                    Thread.Sleep(10);
+                    client.RunCommand($"echo '{passMusic}' | sudo -S reboot");
+
+                    client.Disconnect();
+                    client.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error" + ex);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Обработка нажатия кнопки "Door"
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void DD_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //Данные для подключения по SSH
+            string user = "pi";
+            //string passDoor = GetPassByLogin(user);   :TODO
+            string passDoor = "";
+            string host = fieldLocalIP.Text;
+
+
+            string pathToConfigTemplateDoor = @"..\AvroraAutoConfig\door.py"; //Путь к шаблонному файлу конига датчика дверей
+            List<string> doodDeviceConfig = new List<string>();
+
+
+            using (StreamReader sr = new StreamReader(pathToConfigTemplateDoor, Encoding.Default))
+            {
+                string ip = Dns.GetHostEntry(fieldDns.Text).AddressList[0].ToString(); //Получить ИП адрес по ДНС имени
+                string gateway = ip.Remove(ip.Length - 2) + "1";
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = ParseInputCFG(line, ip, gateway);
+                    doodDeviceConfig.Add(line);
+                }
+            }
+
+
+            string pathOut = @"..\AvroraAutoConfig\doorFull.py";
+            using (StreamWriter wr = new StreamWriter(pathOut, false, System.Text.Encoding.Default))//создаем файл с полним конфигом
+            {
+                int i = 0;
+                while (i < doodDeviceConfig.Count)
+                {
+                    wr.WriteLine(doodDeviceConfig[i]);
+                    i++;
+                }
+
+            }
+            //создаем sftp подключение, загружаем созданый файл на устрофство
+            using (ScpClient client = new ScpClient(host, user, passDoor))
+            {
+                client.Connect();
+                using (Stream localFile = File.OpenRead(pathOut))
+                {
+                    client.Upload(localFile, "/home/pi/doorAuto.py");
+                }
+
+            }
+            //Подключение по SSH, и запуск скрипта для настройки
+            using (var client = new SshClient(host, user, passDoor))
+            {              
+                try
+                {
+                    client.Connect();
+                    Thread.Sleep(1000);
+
+                    var Request = client.RunCommand($@"echo '{passDoor}' | sudo -S python3 doorAuto.py");
+                    Thread.Sleep(100);
+                    client.RunCommand($@"echo '{passDoor}' | sudo -S reboot");
+
+                    Thread.Sleep(500);
+                    client.Disconnect();
+                    client.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error" + ex);
+                }
+            }           
+        }            
+    }
+}
---
+++ AvroraSet/Window1.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using ZXing;
+
+namespace AvroraSet
+{
+    public partial class Window1 : Window
+    {
+        BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.CODE_128 };
+        string settings = "772211002";
+        string saveSettings = "772211003";
+        string cancel = "772211004";
+        string infoBarcode = "772211001";
+        string[] info = new string[8];
+        string dns = "";
+        int i = 0;
+        public Window1(string numTT, string priceNum, string ip, string corpusNum, string inDns)
+        {
+            InitializeComponent();
+            dns = inDns;
+            bool isPing = IsInDomain(dns);
+
+            isInDomain.Content = isPing ? "dns происан в домене" : "dns прдварительно не происан в домене, проверьте пожалуйста";
+
+            info[1] = settings;
+            info[2] = numTT;
+            info[3] = priceNum;
+            info[4] = ip;
+            info[5] = corpusNum;
+            info[6] = saveSettings;
+        }
+
+        /// <summary>
+        /// Генерация изображения из ШК
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        /// <summary>
+        /// Проверка, прописано ли устройство в домене
+        /// </summary>
+        /// <param name="nameOrAddress">ДНС имя</param>
+        /// <returns></returns>
+        public static bool IsInDomain(string nameOrAddress)
+        {
+            try
+            {
+                string ip = Dns.GetHostEntry(nameOrAddress).AddressList[0].ToString();
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки next (Пролистать слайдер вперед)
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (i <= 5)
+            {
+                i++;
+            }
+            else
+            {
+                using (StreamWriter w = new StreamWriter(@"..\AvroraAutoConfig\log.txt", true))
+                {
+                    w.WriteLine(DateTime.Now + " | " + dns + " | " + info[4] + " | " + info[5]);
+                }
+
+                Close();
+            }
+            pic.Source = BitmapToImageSource(writer.Write(info[i]));
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки Cancel (отобразить ШК для отмены настроек)
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            pic.Source = BitmapToImageSource(writer.Write(cancel));
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки Info
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            i = 0;
+            pic.Source = BitmapToImageSource(writer.Write(infoBarcode));
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки Back (Пролистать слайдер назад)
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (i >= 2) i--;
+            pic.Source = BitmapToImageSource(writer.Write(info[i]));
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки ping (Открывает консоль, и начинает пинговать введенный днс адрес)
+        /// </summary>
+        /// <param name="sender">System WPF parametr</param>
+        /// <param name="e">System WPF parametr</param>
+        private void Button_Click_Ping(object sender, RoutedEventArgs e)
+        {
+            string pingData = "ping " + dns;
+
+            var proc1 = new ProcessStartInfo();
+            proc1.UseShellExecute = true;
+
+            proc1.WorkingDirectory = @"C:\Windows\System32";
+
+            proc1.FileName = @"C:\Windows\System32\cmd.exe";
+            proc1.Verb = "runas";
+            proc1.Arguments = "/K " + pingData;
+            proc1.WindowStyle = ProcessWindowStyle.Normal;
+            Process.Start(proc1);
+        }
+    }
+}
